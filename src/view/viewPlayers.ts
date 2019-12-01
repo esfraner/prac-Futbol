efc752f163727cd45ef8518a553bec94a7e30148
esfraner <francisco.encinas.ruiz@gmail.com>
2019-12-01 13:19:11 +0100

    feat(view): create cards players

--- src/view/viewPlayers.ts
+++ src/view/viewPlayers.ts
@@ -12,7 +12,51 @@ export class viewPlayers {
   chargeCards(players: Player[]) {
     players.forEach(player => this.createCard(player));
   }
-  createCard(Player: Player) {
+
+  createPlayerAttributes(player: Player) {
+    const spanNombre = document.createElement("span");
+    const spanAlias = document.createElement("span");
+    const spanId = document.createElement("span");
+    const spanBirthday = document.createElement("span");
+    const textNombre = document.createTextNode(player.name);
+    const textAlias = document.createTextNode(player.alias);
+    const textId = document.createTextNode(player.id);
+    const textBirthday = document.createTextNode(player.birthday);
+  }
+
+  createCard(player: Player) {
+    // <label for="">hola:</label><span>hola</span>
+    // this.createPlayerAttributes(player);
+    const labelNombre = document.createElement("label");
+    const labelAlias = document.createElement("label");
+    const labelId = document.createElement("label");
+    const labelBirthday = document.createElement("label");
+    const textLabelNombre = document.createTextNode("Nombre: ");
+    const textLabelAlias = document.createTextNode("Alias: ");
+    const textLabelId = document.createTextNode("Id: ");
+    const textLabelBirthday = document.createTextNode("Birthday: ");
+
+    const spanNombre = document.createElement("span");
+    const spanAlias = document.createElement("span");
+    const spanId = document.createElement("span");
+    const spanBirthday = document.createElement("span");
+    spanNombre.className = "col s12 m12";
+    spanAlias.className = "col s12 m12";
+    spanId.className = "col s12 m12";
+    spanBirthday.className = "col s12 m12";
+    spanNombre.appendChild(textLabelNombre);
+    spanAlias.appendChild(textLabelAlias);
+    spanId.appendChild(textLabelId);
+    spanBirthday.appendChild(textLabelBirthday);
+    const textNombre = document.createTextNode(player.name);
+    const textAlias = document.createTextNode(player.alias);
+    const textId = document.createTextNode(player.id);
+    const textBirthday = document.createTextNode(player.birthday);
+    spanNombre.appendChild(textNombre);
+    spanAlias.appendChild(textAlias);
+    spanId.appendChild(textId);
+    spanBirthday.appendChild(textBirthday);
+
     const divRow = document.createElement("div");
     divRow.classList.add("row", "card");
     divRow.addEventListener("onclick", function() {
@@ -20,18 +64,15 @@ export class viewPlayers {
     }); //TODO
     //creado un div con card
     const divPlayer = document.createElement("div");
-    divPlayer.className = "col s8 m7";
+    divPlayer.className = "col s6 m7";
+    divPlayer.appendChild(spanId);
+    divPlayer.appendChild(spanNombre);
+    divPlayer.appendChild(spanAlias);
+    divPlayer.appendChild(spanBirthday);
     divRow.appendChild(divPlayer);
-
     const divCard = document.createElement("div");
     divCard.classList.add("player-card");
     divPlayer.appendChild(divCard);
-
-    const p = document.createElement("p");
-    const pText = document.createTextNode("Prueba");
-
-    p.appendChild(pText);
-    divPlayer.appendChild(p);
 
     // const divImage = _view.createElement(ELEMENTS.DIV);
     // const img = _view.createElement(ELEMENTS.IMG);
