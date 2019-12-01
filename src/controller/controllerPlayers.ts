29aa0a0e44402691d70c9b3e294909c343b89702
esfraner <francisco.encinas.ruiz@gmail.com>
2019-12-01 03:01:48 +0100

    feat(view): added createCard function

--- src/controller/controllerPlayers.ts
+++ src/controller/controllerPlayers.ts
@@ -5,12 +5,17 @@ import { PLAYERS } from "../contants/players.mock";
 import { iPlayer } from "../models/player.interface";
 export class controllerPlayers {
   players: Player[];
+  view: viewPlayers;
   constructor(view: viewPlayers, servicePlayer: servicePlayers) {
+    this.view = view;
     this.players = [];
     this.chargePlayers();
     console.log(this.players);
+    this.view.chargeCards(this.players);
   }
   chargePlayers() {
-    PLAYERS.forEach(_player => this.players.push(new Player(_player)));
+    PLAYERS.forEach((_player: iPlayer) =>
+      this.players.push(new Player(_player))
+    );
   }
 }
