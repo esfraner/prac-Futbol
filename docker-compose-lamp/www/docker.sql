207326bad4d7a4215a11c17e23c1dcb224ccc578
Franer <esfraner>
2019-12-04 01:45:07 +0100

    feat(docker): added some images

--- docker-compose-lamp/www/docker.sql
+++ docker-compose-lamp/www/docker.sql
@@ -34,19 +34,21 @@ CREATE TABLE IF NOT EXISTS `PLAYERS` (
   `NAME` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `BIRTHDAY` date NOT NULL,
   `CLUB` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `ROL` enum('PORTERO','DEFENSA','MEDIO','DELANTERO') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'MEDIO'
+  `ROL` enum('PORTERO','DEFENSA','MEDIO','DELANTERO') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'MEDIO',
+  `IMAGE` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `PLAYERS`
 --
 
-INSERT INTO `PLAYERS` (`ID`, `ALIAS`, `NAME`, `BIRTHDAY`, `CLUB`, `ROL`) VALUES
-('25', 'NEYMAR JR', 'Neymar da Silva Santos Júnior', '1992-02-05', 'Real Madrid', 'DELANTERO'),
-('44', 'SILVA', 'Thiago Emiliano da Silva', '1984-09-22', 'FC Malaga', 'MEDIO'),
-('27', 'RONALDINHO', 'Ronaldo de Assis Moreira', '1986-07-22', 'FC Barcelona', 'DELANTERO'),
-('29', 'KILLER', 'Pepe Képler Laveran Lima Ferreira', '1990-09-11', 'FC Porto', 'DEFENSA'),
-('89', 'Iker', 'Iker Casillas Fernández', '1997-09-11', 'FC Porto', 'PORTERO');
+INSERT INTO `PLAYERS` (`ID`, `ALIAS`, `NAME`, `BIRTHDAY`, `CLUB`, `ROL`,`IMAGE`) VALUES
+('25', 'NEYMAR JR', 'Neymar da Silva Santos Júnior', '1992-02-05', 'Real Madrid', 'DELANTERO','http://localhost/assets/images/neymar.jpg'),
+('44', 'SILVA', 'Thiago Emiliano da Silva', '1984-09-22', 'FC Malaga', 'MEDIO','http://localhost/assets/images/ronaldinho.jpg'),
+('45', 'CR7', 'Cristiano Ronaldo', '1984-09-22', 'Real Madrid', 'DELANTERO','http://localhost/assets/images/cristiano.jpg'),
+('27', 'RONALDINHO', 'Ronaldo de Assis Moreira', '1986-07-22', 'FC Barcelona', 'DELANTERO','http://localhost/assets/images/ronaldinho.jpg'),
+('29', 'KILLER', 'Pepe Képler Laveran Lima Ferreira', '1990-09-11', 'FC Porto', 'DEFENSA','http://localhost/assets/images/cristiano.jpg'),
+('89', 'Iker', 'Iker Casillas Fernández', '1997-09-11', 'FC Porto', 'PORTERO','http://localhost/assets/images/cristiano.jpg');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
